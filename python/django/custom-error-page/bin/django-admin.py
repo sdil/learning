254190885b47fe1dc769f6d66fe3e71486aaee3b
Fadhil Yaacob <mohamadfadhil.yaacob@onapp.com>
2020-07-01 20:54:01 +0800

    Add custom error page lesson

---
+++ python/django/custom-error-page/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/fadhil/personal-project/learning/python/django/project1/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
