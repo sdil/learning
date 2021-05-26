f5076920e53ed689d23db52e074d23dce005dc70
Fadhil Yaacob <mohamadfadhil.yaacob@onapp.com>
2021-05-26 16:58:15 +0800

    remove deps files

--- python/django/custom-error-page/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/fadhil/personal-project/learning/python/django/project1/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
